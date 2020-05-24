06419eda8cad69efdcc147f3b68d525a083e533c
TMBuonocore <t.m.buonocore@amc.uva.nl>
2020-05-24 14:31:34 +0200

    introduction

--- ui.R
+++ ui.R
@@ -51,6 +51,31 @@ ui <- fluidPage(
               tags$u(a("Detsutut - 2020",href="https://github.com/detsutut"))))
       ),
       dashboardBody(
+        div(id="introContainer", style="background-image: url(texture.jpg); background-repeat: repeat;",
+        div(id="introduction",
+            div(id ="introText", style="text-align: center; padding: 10px 10px 10px 10px;",
+            h4("Il tuo tridente da sogno ti ha lasciato fuori dal podio?"),
+            h4("La tua media di 4 gol a partita non ha impedito al tuo dream team di subire più gol del Benevento?"),
+            h4("Hai comprato Higuain nell'anno del record e sei riuscito comunque ad arrivare secondo?"),
+            br(),
+            h4("Scopri quanto sei sfigato con il ",tags$b("Fantasimulatore!"))
+            )
+        ),
+        br(),
+        div(id="introduction2",
+            div(id ="introText2", style="text-align: center; padding: 10px 10px 10px 10px;",
+                h5("Per vincere il fantacalcio serve un mix di bravura, pianificazione, talent scouting e...fortuna. Tanta fortuna."),
+                h5("Costruire la rosa più forte possibile talvolta può non essere sufficiente di fronte ad un calendario poco favorevole, dove roboanti pareggi 4-4 si affiancano a risicate vittorie per 1-0."),
+                h5("Spesso, infatti, la disposizione delle squadre ai blocchi di partenza quando si genera il calendario del campionato si rivela fondamentale per la classifica finale, più degli effettivi risultati ottenuti sul campo."),
+                br(),
+                h5("Lo scopo di ",tags$b("Fantasimulatore"),"è quello di rimuovere il Fattore C - dove C non sta per Calendario - dal fantacalcio andando a calcolare tutti i possibili sorteggi di calendario e simulando l'esito del campionato per ciascuno di essi.")
+            )
+        ),
+        br(),
+        div(id="introduction3", style="align: center;",
+            img(src="example3.png",width="90%",style="  display: block;margin-left: auto;margin-right: auto;")
+        )
+        ),
         plotlyOutput("plot"),
         plotlyOutput("plot2")
       )
